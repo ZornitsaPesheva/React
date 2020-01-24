4077dae026a8f1e3a044c2dfb807078475c39a16
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-01-24 12:12:12 +0200

    node added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,19 @@ export default class App extends Component {
         <OrgChart nodes={
           [{id: 1, name: "Name1", title: "Tytle1"}, 
           {id: 2, pid: 1, name: "Name2", title: "Tytle2"},
-          {id: 3, pid: 1, name: "Name3", title: "Tytle3"}]
+          {id: 3, pid: 1, name: "Name3", title: "Tytle3"},
+          {id: 4, pid: 2, name: "Name3", title: "Tytle3"},
+          {id: 5, pid: 2, name: "Name3", title: "Tytle3"},
+          {id: 6, pid: 3, name: "Name3", title: "Tytle3"},
+          {id: 7, pid: 3, name: "Name3", title: "Tytle3"},
+          {id: 8, pid: 4, name: "Name3", title: "Tytle3"},
+          {id: 9, pid: 4, name: "Name3", title: "Tytle3"},
+          {id: 10, pid: 5, name: "Name3", title: "Tytle3"},
+          {id: 11, pid: 5, name: "Name3", title: "Tytle3"},
+          {id: 12, pid: 6, name: "Name3", title: "Tytle3"},
+          {id: 13, pid: 6, name: "Name3", title: "Tytle3"},
+          {id: 14, pid: 7, name: "Name3", title: "Tytle3"},
+          {id: 15, pid: 7, name: "Name3", title: "Tytle3"}]
         }/>
       </div>
     );
